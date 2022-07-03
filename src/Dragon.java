a6b346d2d4afc12d6b6bcd52903947a908c4afd0
sona91shoney <108096038+sona91shoney@users.noreply.github.com>
2022-07-03 19:59:47 -0400

    Initial commit

---
+++ src/Dragon.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Dragon {
+    public static void main(String[] args) {
+        System.out.println("You are in a land full of dragons. In front of you, \n" +
+                "you see two caves....\n" +
+                "In one cave,"+ "the dragon is friendly \n " +
+                "and will share his treasure with you." + "The other dragon \n" +
+                "is greedy and hungry and will eat you on sight.");
+        System.out.println("Which cave will you go into? (1 or 2)");
+        System.out.println("--------------------------------------");
+        Scanner xx = new Scanner(System.in);
+
+        int option = xx.nextInt();
+        if (option == 1) { // If user selects cave 1
+            System.out.println("You approach the cave...\n" +
+                    "It is dark and spooky...\n" +
+                    "A large dragon jumps out in front to you! He opens his jaws and ...\n" +
+                    "Gobbles you down in one bite!");
+        } else if (option == 2) { // If user selects cave 2
+            System.out.println("Friendly Dragon and would like to share his treasure with you!");
+        } else {
+            System.out.println("You are lost!");
+        }
+    }
+}
